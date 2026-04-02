607e4b11b93a2b97f7717c7ad901f44ca6dd54ff
Eslam m.gomaa <eslsmgomaa47@gmail.com@gmail.com>
2026-04-02 15:21:59 +0200

    kkkk

--- rucurcion.cpp
+++ rucurcion.cpp
@@ -12,16 +12,15 @@ using namespace std;
 // {
 //    print_numbers(1,100);
 // }
-void print_number(int x,int y)
+void print_numbers(int n,int m)
 {
-    if(x>=y)
+    if(n>=m)
     {
-        cout<<x<<" "<<endl;
-        print_number(x-1,y);
+        cout<<n<<" "<<endl;
+        print_numbers(n-1,m);
     }
+}   
+int main()
+{
+   print_numbers(100,1);
 }
-    int main()
-    {
-        print_number(100,1);
-        return 0;
-    }
